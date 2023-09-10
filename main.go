86d514aa67b37bc43ea3d8df81545f2f5bdff14a
felix-ha <hauerf98@gmail.com>
2023-09-10 21:41:41 +0200

    Implement core logic

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func isPalindrome(s string) bool {
-	s = strings.ToLower(s)
+	length := len(s)
+
+	for i := 0; i < length/2; i++ {
+		if s[i] != s[length-1-i] {
+			return false
+		}
+	}
+
 	return true
 }
 
 func main() {
 	fmt.Println(isPalindrome("racecar"))
+	fmt.Println(isPalindrome("otto"))
+
+	fmt.Println(isPalindrome("abc"))
 }
--- main_test.go
+++ main_test.go
@@ -2,9 +2,30 @@ package main
 
 import "testing"
 
-func TestExample(t *testing.T) {
+func TestExampleTrueEven(t *testing.T) {
 	result := isPalindrome("anna")
 	if result == false {
 		t.Errorf("Expected result to be true, got false")
+	}
+}
+
+func TestExampleTrueOdd(t *testing.T) {
+	result := isPalindrome("rentner")
+	if result == false {
+		t.Errorf("Expected result to be true, got false")
+	}
+}
+
+func TestExampleFalseEven(t *testing.T) {
+	result := isPalindrome("abcd")
+	if result == true {
+		t.Errorf("Expected result to be false, got true")
+	}
+}
+
+func TestExampleFalseOdd(t *testing.T) {
+	result := isPalindrome("abcde")
+	if result == true {
+		t.Errorf("Expected result to be false, got true")
 	}
 }
